939790627d50aa1c8485fb696bc598ce0dae950d
jufegare000 <juanfgallo@experimentality.co>
2022-01-04 21:25:44 -0500

    mocha configured

---
+++ tests/unit/example.test.ts
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+
+describe('Options tests', () => { // the tests container
+    it('checking default options', () => { // the single test
+        console.log("pare pues cucho")
+        console.log("holi")
+    });
+});
