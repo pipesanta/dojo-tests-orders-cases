0814756b276c472420bd343b50095fc36a084546
jufegare000 <juanfgallo@experimentality.co>
2022-01-05 20:38:29 -0500

    refactoring mock folder!

--- tests/unit/example.test.ts
+++ tests/unit/example.test.ts
@@ -1,7 +1,5 @@
 
 import { calculateDiscountTwoProductsWithPricegreaterThat100k } from '../../src/BusinessLogic/discounts';
-import { sayHello } from '../../src/BusinessLogic/helloworld'
-import { Order } from '../../src/Interfaces/Order';
 import { orderMock } from '../mocks/order.mock';
 
 import { expect } from 'chai';
@@ -9,8 +7,6 @@ import { expect } from 'chai';
 // ESTE ES SOLO UN EJEMPLO YA QUE ME FALTAN PONER MÁS CASOS POSIBLES, POR EJEMPLO, ENVIAR VARIOS PRODUCTOS QUE SUPEREN EN TOTAL LOS 100000
 describe('Discount cheapest product', () => { // the tests container
     it('Just one product with unit price greater than 100000', () => { // the single test
-
-
         let actualResult: number = calculateDiscountTwoProductsWithPricegreaterThat100k(orderMock);
         expect(actualResult).to.equal(200000);
     });
