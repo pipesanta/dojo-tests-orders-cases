3cb9236da4d14afc52f0fbef70354ca12c60afcc
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2022-01-06 00:38:08 -0500

    Bussiness logic discount 30% to product with the cheaper unit price done

--- src/BusinessLogic/discounts.ts
+++ src/BusinessLogic/discounts.ts
@@ -1,12 +1,12 @@
 
 import { Order } from '../Interfaces/Order'; 
 
-// Por la compra de dos productos o más, que cada uno supere 
-// el valor de 100.000, se otorgará un 30% de descuento en el producto con el menor valor.
-export const calculateDiscountTwoProductsWithPricegreaterThat100k = ( order: Order ): number => {  
+// Por la compra de dos productos distintos o más, y además si la compra supera el valor de 100.000, 
+// se otorgará un 30% de descuento en el producto con menor precio unitario.
+export const calculateDiscountTwoProductsWithPurchasesgreaterThan100k = ( order: Order ): number => {  
     let finalPriceCheaperValue: number;
     let total: number = order.products.reduce( (totalOrder, item) =>{
-        return totalOrder += item.unitPrice;
+        return totalOrder += item.unitPrice * item.quantity;
     }, 0);
     order.products.sort( (a , b)=> (a.unitPrice > b.unitPrice ? 1 : -1));
     let priceCheaperValue: number = order.products[0].unitPrice;
@@ -17,5 +17,3 @@ export const calculateDiscountTwoProductsWithPricegreaterThat100k = ( order: Ord
     }
     return finalPriceCheaperValue;
 }
-
- 
--- tests/mocks/order.mock.ts
+++ tests/mocks/order.mock.ts
@@ -1,11 +1,11 @@
 import { Order } from "../../src/Interfaces/Order";
 
-export  const orderMock: Order = {
+const orderMockOneProduct: Order = {
     orderId: "A92B-01",
     products: [{
         productId: 1,
         categoryId: 6,
-        unitPrice: 200000,
+        unitPrice: 60000,
         quantity: 2
     }
     ],
@@ -16,3 +16,81 @@ export  const orderMock: Order = {
         city: "Medellin"
     }
  }
+
+const orderMockTwoProductsGreaterThan100k: Order = {
+    orderId: "A92B-01",
+    products: [{
+        productId: 1,
+        categoryId: 6,
+        unitPrice: 40000,
+        quantity: 2
+    },
+    {
+        productId: 1,
+        categoryId: 6,
+        unitPrice: 20100,
+        quantity: 1
+    }
+    ],
+    client: {
+        email: "juanguti@gmail.com",
+        phone: "+573018879988",
+        address: "Calle 40# 66b - 12",
+        city: "Medellin"
+    }
+ }
+
+ 
+
+ const orderMockTwoProductsGreaterThan100kDiffSort: Order = {
+    orderId: "A92B-01",
+    products: [{
+        productId: 1,
+        categoryId: 6,
+        unitPrice: 20100,
+        quantity: 1
+    },{
+        productId: 1,
+        categoryId: 6,
+        unitPrice: 40000,
+        quantity: 2
+    }
+    
+    ],
+    client: {
+        email: "juanguti@gmail.com",
+        phone: "+573018879988",
+        address: "Calle 40# 66b - 12",
+        city: "Medellin"
+    }
+ }
+
+ const orderMockTwoProductsLessOrEqualThan100k: Order = {
+    orderId: "A92B-01",
+    products: [{
+        productId: 1,
+        categoryId: 6,
+        unitPrice: 40000,
+        quantity: 2
+    },
+    {
+        productId: 1,
+        categoryId: 6,
+        unitPrice: 19000,
+        quantity: 1
+    }
+    ],
+    client: {
+        email: "juanguti@gmail.com",
+        phone: "+573018879988",
+        address: "Calle 40# 66b - 12",
+        city: "Medellin"
+    }
+ }
+
+export {
+    orderMockOneProduct,
+    orderMockTwoProductsGreaterThan100k,
+    orderMockTwoProductsGreaterThan100kDiffSort,
+    orderMockTwoProductsLessOrEqualThan100k
+}
